fea6cd5d0ac742a38e2b5e1a4727a03bbbbd414b
stnikolas <bebagi61@gmail.com>
2023-12-02 23:21:45 +0000

    removed modules csv's

--- srs/users/Student.java
+++ srs/users/Student.java
@@ -7,16 +7,18 @@ public class Student{
 
 
     public static String showTranscript(String studentID) {
+        // Todo: implement
         System.out.println("show a transcript selected");
         return "transcript";
     }
 
-    public void calculateQSA() {
+    public void calculateQSA(String studentID) {
+        // TODO implement
         System.out.println("calculate QSA selected");
     }
 
     public static List<String> showStudentModules(String studentID) {
-       List<String> modules = new ArrayList<String>();
+       List<String> modules = new ArrayList<>();
         System.out.println("show modules selected");
         return modules;
     }
